fcb43d1a5d4a339156637e4d1a59b33628cda9f0
Joao Manoel <joaomanoellima98@gmail.com>
2021-01-17 13:24:02 -0300

    Adicionado esqueleto e texto base

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import logo from './assets/logo.png';
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-        </p>
+        <img src={logo} className="App-logo" alt=""/>
+        <h1>Baixar o app</h1>
+        <h1>Soluções para sua empresa</h1>
+        <h1>Loja</h1>
+        <h1>Serviços</h1>
+        <h1>Sobre</h1>
+        <h1>Login</h1>
       </header>
+
+      <h1>Soluções de reciclagem para sua empresa</h1>
+      <p>Auxiliamos empresas a se manterem de acordo com as políticas ambientais, enquanto construímos um futuro mais digno para os jovens</p>
+
+      
     </div>
   );
 }
